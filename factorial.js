2076b2f2c4f9b1741c05f9efdecf6c7cbfc111b0
Edwinkib <kiberu@esolutionsug.com>
2023-02-21 11:13:28 +0300

    update 3

--- factorial.js
+++ factorial.js
@@ -1,15 +1,32 @@
-const factorial = (num) => {
-  let result = 1;
+// const factorial = (num) => {
+//   let result = 1;
 
-  // eslint-disable-next-line eqeqeq
-  if (num == 0 || num == 1) {
-    return result;
-  }
+//   // eslint-disable-next-line eqeqeq
+//   if (num == 0 || num == 1) {
+//     return result;
+//   }
+//   // eslint-disable-next-line no-plusplus
+//   for (let i = num; i >= 1; i--) {
+//     result *= i;
+//   }
+//   return result;
+// };
+
+// Factorial of n is the product of all positive descending integers.
+// Factorial of n is denoted by n!. For example:
+// 4! = 4*3*2*1 = 24
+// 5! = 5*4*3*2*1 = 120
+
+// eslint-disable-next-line no-unused-vars
+function Factorial(n) {
+  let ans = 1;
+
   // eslint-disable-next-line no-plusplus
-  for (let i = num; i >= 1; i--) {
-    result *= i;
-  }
-  return result;
-};
+  for (let i = 2; i <= n; i++) { ans *= i; }
+  // eslint-disable-next-line no-console
+  console.log(ans);
+  return ans;
+}
 
-module.exports = factorial;
+Factorial(5);
+// module.exports = factorial;
